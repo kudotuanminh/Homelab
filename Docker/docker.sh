5f8e87f7f7c89650d6ed0bab781245559c68e1a2
Tuan Minh Ngo <catboom01@gmail.com>
2024-05-15 00:10:38 +0700

    Update

--- Docker/docker.sh
+++ Docker/docker.sh
@@ -1,7 +1,3 @@
-docker stop endlessh
-docker rm endlessh
-docker pull shizunge/endlessh-go:latest
-docker run -d -p 2112:2112 -p 2222:2222 --name endlessh --restart=unless-stopped shizunge/endlessh-go:latest -interval_ms 10000 -geoip_supplier ip-api -enable_prometheus -logtostderr -v=1
 docker stop demergi-dpi
 docker rm demergi-dpi
 docker pull docker.io/hectorm/demergi:latest
