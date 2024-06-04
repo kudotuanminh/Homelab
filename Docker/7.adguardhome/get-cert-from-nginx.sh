5549adec05b2a953c05e574093ecc00b44973f73
Tuan Minh Ngo <catboom01@gmail.com>
2024-06-04 14:30:29 +0700

    Update vmagent for cAdvisor, update AdguardHome for DoH

---
+++ Docker/7.adguardhome/get-cert-from-nginx.sh
@@ -0,0 +1,2 @@
+sudo cp ../0.nginx-proxy-manager/data/letsencrypt/live/npm-8/privkey.pem ./data/letsencrypt
+sudo cp ../0.nginx-proxy-manager/data/letsencrypt/live/npm-8/fullchain.pem ./data/letsencrypt
--- Docker/docker.sh
+++ Docker/docker.sh
@@ -1,5 +1,5 @@
+docker pull docker.io/hectorm/demergi:latest
 docker stop demergi-dpi
 docker rm demergi-dpi
-docker pull docker.io/hectorm/demergi:latest
-docker run -d -p 8080:8080 --name demergi-dpi --restart=unless-stopped docker.io/hectorm/demergi:latest
+docker run -d -p 8080:8080 -e DEMERGI_WORKERS=4 -e DEMERGI_DOH_URL='https://adguard.local.minhnt.net/dns-query' --name demergi-dpi --restart=unless-stopped docker.io/hectorm/demergi:latest
 docker image prune -a -f
